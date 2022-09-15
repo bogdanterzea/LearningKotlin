3c7dd24d7884e75bee1df8652beb5bef2bce1371
Terzea Bogdan <terzeabogdan2007@yahoo.com>
2022-09-15 14:52:59 +0300

    add solution

--- app/src/main/java/com/example/learningapps/MainActivity.kt
+++ app/src/main/java/com/example/learningapps/MainActivity.kt
@@ -3,9 +3,26 @@ package com.example.learningapps
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+fun main() {
+    println(hasRepeatedCharacter("abc")) // false"
+
+    println(hasRepeatedCharacter("aabc")) // true
+
+    println(hasRepeatedCharacter("aabcc")) // true
 }
+
+fun hasRepeatedCharacter (string : String): Boolean {
+    val chars: List<Char> = string.toList()
+
+    var character = chars[0]
+
+    chars.forEach { param ->
+        val result = string.count {
+            it == param
+        } > 1
+        if(result) return result
+    }
+
+    return false
+}
+
