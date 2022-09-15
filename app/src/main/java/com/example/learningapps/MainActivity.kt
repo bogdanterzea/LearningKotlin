dfca6517d1651ad8330ea11d0fe70b535fccff29
Terzea Bogdan <terzeabogdan2007@yahoo.com>
2022-09-15 14:20:31 +0300

    my solution

--- app/src/main/java/com/example/learningapps/MainActivity.kt
+++ app/src/main/java/com/example/learningapps/MainActivity.kt
@@ -3,9 +3,24 @@ package com.example.learningapps
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+fun main() {
+    println(squareEquals(listOf(1, 2, 3), listOf(9, 1, 4)))
+    squareEquals(listOf(1, 2, 3), listOf(9, 1, 4))
+}
+
+fun squareEquals(list1: List<Int>, list2: List<Int>): Boolean {
+    var result = true
+
+    if (list1.size != list2.size)
+        result = false
+    val list3: List<Int> = list1.map {
+        it * it
     }
+
+    if (!list3.containsAll(list2) || !list2.containsAll(list3)) {
+        result = false
+    }
+
+    return result
+
 }
